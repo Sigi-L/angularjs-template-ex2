a3287f37b243b2a26cecc1369eb1b573ccb0f259
sigi-l <sigi.lahiany@gmail.com>
2018-03-24 00:38:25 +0300

    Correct menu (actors, movies (empty)

--- exDrct.js
+++ exDrct.js
@@ -1,5 +1,11 @@
-myApp.directive('mynavbar', function() {
-    return {
-      templateUrl: 'mynavbar.html'
-    };
+myApp.directive('mynavbar', function () {
+  return {
+    templateUrl: 'mynavbar.html'
+  };
+});
+
+myApp.directive('actorsDrct', function () {
+  return {
+    templateUrl: 'actorsDrct.html'
+  };
 });
